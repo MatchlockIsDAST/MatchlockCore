d0fb799493713725377589722b89a950b8ddbc0b
WestEast1st <bhns1270132@gn.iwasaki.ac.jp>
2019-11-23 15:08:18 +0900

    Fetchを追加

--- src/domain/model/request/request.go
+++ src/domain/model/request/request.go
@@ -26,3 +26,11 @@ func New(req *http.Request) (request *Request) {
 	request.Body = entity.CreateBody(req.Header.Get("Content-Type"), req.Body, req.Form)
 	return request
 }
+
+//Fetch Request内部のデータを*http.Requestに変換して取得します
+func (r *Request) Fetch() *http.Request {
+	r.Request.URL = r.URL.Fetch()
+	r.Request.Header = r.Header.Fetch()
+	r.Request.Body = r.Body.Fetch()
+	return r.Request
+}
