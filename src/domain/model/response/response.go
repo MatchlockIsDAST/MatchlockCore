3e4003901e86d561572230eec2d9378b701c4efe
WestEast1st <bhns1270132@gn.iwasaki.ac.jp>
2019-12-11 22:17:28 +0900

    レスポンスの追加

---
+++ src/domain/model/response/response.go
@@ -0,0 +1,39 @@
+package response
+
+import (
+	"net/http"
+
+	"github.com/MatchlockIsDAST/httpmesconv/stringto"
+
+	"github.com/MatchlockIsDAST/httpmesconv/tostring"
+
+	"github.com/MatchlockIsDAST/MatchlockCore/src/domain/model/request"
+)
+
+//Response Core内部で利用するHTTP Response の定義です
+type Response struct {
+	*http.Response
+	Header http.Header
+	Body   string
+	*request.Request
+}
+
+//New *model.Response を返します
+func New(resp *http.Response) (response *Response) {
+	response = &Response{
+		Response: resp,
+		Header:   resp.Header,
+		Body:     tostring.Body(resp.Body),
+		Request:  request.New(resp.Request),
+	}
+	return response
+}
+
+//Fetch Response 内部のデータを*http.Requestに変換して取得します
+func (r *Response) Fetch() *http.Response {
+	response := r.Response
+	response.Header = r.Header
+	response.Body = stringto.IoReadCloser(r.Body)
+	response.Request = r.Request.Fetch()
+	return response
+}
