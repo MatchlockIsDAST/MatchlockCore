c4fc65ada9e3a57e6b9f17a3b4d90fa21f354226
WestEast1st <bhns1270132@gn.iwasaki.ac.jp>
2019-12-17 23:10:50 +0900

    auditinfoを追加

---
+++ src/domain/model/auditinfo/auditinfo.go
@@ -0,0 +1,54 @@
+package auditinfo
+
+import (
+	"net/http"
+
+	"github.com/MatchlockIsDAST/MatchlockCore/src/domain/model/auditinfo/value"
+)
+
+//AuditInfo 検査を行うための情報をまとめた構造体
+type AuditInfo struct {
+	Server        []string
+	ReplaceString string
+	AuthInfo      *value.AuthInfomation
+	Points        []*AuditPoint
+}
+
+//NewAuditInfo PointとサーバーをまとめたAuditInfoを返す
+func NewAuditInfo(server []string, replace string, points ...*AuditPoint) (info *AuditInfo) {
+	info.Server = server
+	info.ReplaceString = replace
+	if len(points) != 0 {
+		info.Points = points
+	}
+	return info
+}
+
+//SetAuth 認証情報を設定します
+func (audit *AuditInfo) SetAuth(authinfo *value.AuthInfomation) {
+	audit.AuthInfo = authinfo
+}
+
+//AddPoint EndPointを追加することができます
+func (audit *AuditInfo) AddPoint(points ...*AuditPoint) {
+	audit.Points = append(audit.Points, points...)
+}
+
+//AuditPoint Path(endpoint)を定義した構造体
+type AuditPoint struct {
+	Method,
+	Path,
+	ContentType string
+	Header http.Header
+	value.ParamInfomation
+}
+
+//NewAuditPoint 検査を行うエンドポイントを入力します
+func NewAuditPoint(method, path, contenttype string, header http.Header) *AuditPoint {
+	return &AuditPoint{
+		Method:      method,
+		Path:        path,
+		ContentType: contenttype,
+		Header:      header,
+	}
+}
